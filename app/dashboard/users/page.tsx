2816677d521066acce8f50c633f47c717b7655ac
hesand <helge.sandvei@gmail.com>
2025-11-12 21:31:08 +0100

    adding user Helge Sandvei

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -5,7 +5,7 @@ const Users = () => {
     <div>
       <h1>Dashboard Users</h1>
       <ul>
-        <li><Link href="/dashboard/users/1">Helge</Link></li>
+        <li><Link href="/dashboard/users/1">Helge Sandvei</Link></li>
         <li><Link href="/dashboard/users/2">Users 2</Link></li>
         <li><Link href="/dashboard/users/3">Users 3</Link></li>
         <li><Link href="/dashboard/users/4">Users 4</Link></li>
